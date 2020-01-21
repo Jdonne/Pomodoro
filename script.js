9b9a3de2246ea4e9fcfbbf2483b7e7d0a34d9db0
Eric Tien <eric.tien.13@gmail.com>
2020-01-21 15:01:14 -0500

    add style and change phase color

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+//try const the minutes and seconds
+
+const body = document.querySelector('body');
 const display = document.querySelector('.display');
-const status = document.querySelector('.status')
+const status = document.querySelector('.status');
 //buttons
 const workButton = document.querySelector('.work');
 const breakButton = document.querySelector('.break');
@@ -19,54 +22,59 @@ status.textContent = "WORK TIME"
 
 function countdown() {
     count -= 1;
-    minutes = ('00' + parseInt(count/60)).substr(-2);
-    seconds = ('00' + count%60).substr(-2);
-    display.textContent = minutes + ':'+seconds;
-if (count == 0 && phase ==0){
-    phase = 1;
-    count = 1500;
-    status.textContent = "BREAK TIME"
+    minutes = ('00' + parseInt(count / 60)).substr(-2);
+    seconds = ('00' + count % 60).substr(-2);
+    display.textContent = minutes + ':' + seconds;
+    if (count == 0 && phase == 0) {
+        phase = 1;
+        count = 300;
+        status.textContent = "BREAK TIME"
+        body.classList.add('breakcolor');
+    }
+    else if (count == 0 && phase == 1) {
+        phase = 0;
+        count = 1500;
+        status.textContent = "WORK TIME";
+        body.classList.remove('breakcolor');
+    }
 }
-else if (count == 0 && phase == 1){
-    phase = 0;
-    count = 300;
-    status.textContent = "WORK TIME"
-}
-}    
 
 
 function timer(e) {
-    if (e.target ==workButton) {
-        clearInterval(interval)
-        phase = 0
-        count = 1500
-        display.textContent = "25:00"
-        status.textContent = "WORK TIME"
+    if (e.target == workButton) {
+        clearInterval(interval);
+        phase = 0;
+        count = 1500;
+        display.textContent = "25:00";
+        status.textContent = "WORK TIME";
+        body.classList.remove('breakcolor');
     }
-    else if (e.target == breakButton){
-        clearInterval(interval)
-        phase = 1
-        count =300
-        display.textContent = "05:00"
-        status.textContent = "BREAK TIME"
+    else if (e.target == breakButton) {
+        clearInterval(interval);
+        phase = 1;
+        count = 300;
+        display.textContent = "05:00";
+        status.textContent = "BREAK TIME";
+        body.classList.add('breakcolor');
     }
-    
-   else if (e.target == startButton){
-        interval = setInterval(countdown, 1000);}
+
+    else if (e.target == startButton) {
+        interval = setInterval(countdown, 1000);
+    }
 }
 
 function stopReset(e) {
-clearInterval(interval);
-if (e.target == resetButton) {
-    if (phase == 0){
-count = 1500;
-display.textContent = "25:00"
-}
-else if (phase == 1){
-    count = 300
-    display.textContent = "05:00"
-}
-}
+    clearInterval(interval);
+    if (e.target == resetButton) {
+        if (phase == 0) {
+            count = 1500;
+            display.textContent = "25:00"
+        }
+        else if (phase == 1) {
+            count = 300
+            display.textContent = "05:00"
+        }
+    }
 }
 workButton.addEventListener('click', timer);
 breakButton.addEventListener('click', timer);
